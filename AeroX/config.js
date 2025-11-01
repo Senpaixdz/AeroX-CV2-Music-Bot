088a9722868b9da74a80374ab1f07658387ccd01
Senpaixdz <deadlordeditz12@gmail.com>
2025-11-01 19:54:02 +0530

    Update config.js

--- AeroX/config.js
+++ AeroX/config.js
@@ -1,16 +1,16 @@
 
 module.exports = {
-    BOT_TOKEN: process.env.BOT_TOKEN || '',
-    CLIENT_ID: process.env.CLIENT_ID || '',
-    OWNER_ID: process.env.OWNER_ID || '',
+    BOT_TOKEN: process.env.BOT_TOKEN || 'MTI5MDMxMDk5ODUzMTUwNjE5Ng.GYyf1z.Fc5IPlJnnZtPFhPyVPmn6j3qIIkN757B-O9gqw',
+    CLIENT_ID: process.env.CLIENT_ID || '1290310998531506196',
+    OWNER_ID: process.env.OWNER_ID || '440498587357413388',
     PREFIX: ',',
     
     
     LAVALINK: {
-        HOSTS: process.env.LAVALINK_HOSTS || '',
-        PORTS: process.env.LAVALINK_PORTS || '',
-        PASSWORDS: process.env.LAVALINK_PASSWORDS || '',
-        SECURES: process.env.LAVALINK_SECURES || ''
+        HOSTS: process.env.LAVALINK_HOSTS || 'nexus.voidhosting.vip',
+        PORTS: process.env.LAVALINK_PORTS || '6004',
+        PASSWORDS: process.env.LAVALINK_PASSWORDS || 'cocaine',
+        SECURES: process.env.LAVALINK_SECURES || 'false'
     },
     
     
@@ -23,8 +23,8 @@ module.exports = {
     
     
     SPOTIFY: {
-        CLIENT_ID: process.env.SPOTIFY_CLIENT_ID || '',
-        CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET || ''
+        CLIENT_ID: process.env.SPOTIFY_CLIENT_ID || '257d3625d6504e3980538279105fdf09',
+        CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET || '6c7db5ff6a724285a468a5d98b383b54'
     },
     
     
